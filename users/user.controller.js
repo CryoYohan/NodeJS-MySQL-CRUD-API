72e5baf274168bf63c76323be1eefb1e258e58ec
itsmecyril <cyrilypil@gmail.com>
2025-03-07 18:18:15 +0800

    added services

---
+++ users/user.controller.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const validateRequest = require('_middleware/validate-request');
+const Role = require('_helpers/role');
+const userService = require('users/user.service');
+
+// routes
+router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', createSchema, create);
+router.put('/:id', updateSchema, update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+//route functions
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(next);
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => res.json(user))
+        .catch(next);
+}
